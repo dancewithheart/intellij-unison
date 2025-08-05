8426166a0a4f96565ebdab10055dc8282e4c11e4
dancewithheart <dancingwithopenheart@gmail.com>
2025-08-05 10:25:28 +0200

    fix tests after BNF - tests for pattern matching

---
+++ src/test/scala/intellij/unison/PatternMatchingTest.scala
@@ -0,0 +1,18 @@
+package intellij.unison
+
+class PatternMatchingTest extends BaseUnisonTest {
+
+  def testLiteralPatterns(): Unit = runTests()
+
+  def testAsPatterns(): Unit = runTests()
+
+  def testConstructorPatterns(): Unit = runTests()
+
+  def testListPatterns(): Unit = runTests()
+
+  def testTuplePatterns(): Unit = runTests()
+
+  def testPatternMatchGuards(): Unit = runTests()
+
+  def testCasesSyntax(): Unit = runTests()
+}
