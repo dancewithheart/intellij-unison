04e2ec3e1a3b457a78d8614df8c3140a5b230625
dancewithheart <dancingwithopenheart@gmail.com>
2025-08-05 10:34:05 +0200

    fix tests after BNF - split UseClause tests

--- src/test/scala/intellij/unison/UseClauseTest.scala
+++ src/test/scala/intellij/unison/UseClauseTest.scala
@@ -1,6 +1,9 @@
 package intellij.unison
 
-class UseClauseTest extends BaseUnisonTest {
+class UseClauseTest
+    extends BaseUnisonTest {
 
   def testUseClause(): Unit = runTests()
+
+  def testUseClauseOperator(): Unit = runTests()
 }
