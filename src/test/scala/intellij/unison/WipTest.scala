7faeebe0cf2bae75cadc2cde6ad453cc211c0804
Piotr Paradziński <dancingwithopenheart@gmail.com>
2025-08-12 14:11:40 +0200

    Reorg pattern matching gards tests, update readme: WIP warning (#44)
    * reorganize pattern matching guards tests
    
    * update readme with polymorphic types, pattern matching

--- src/test/scala/intellij/unison/WipTest.scala
+++ src/test/scala/intellij/unison/WipTest.scala
@@ -21,8 +21,6 @@ class WipTest
 
   def testExceptionsHandling(): Unit = runTests()
 
-  def testPatternMatchGuards1(): Unit = runTests()
-
   def testPatternMatchGuards2(): Unit = runTests()
 
   def testPatternMatchGuards3(): Unit = runTests()
