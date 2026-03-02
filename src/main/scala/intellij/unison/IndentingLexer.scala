dfb1dd090c32b39827eb94558a4c07608bb85bb4
dancewithheart <dancingwithopenheart@gmail.com>
2026-03-02 21:39:47 +0100

    More Scala idiomatic IndentingLexer

--- src/main/scala/intellij/unison/IndentingLexer.scala
+++ src/main/scala/intellij/unison/IndentingLexer.scala
@@ -5,52 +5,66 @@ import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import intellij.unison.language.psi.UnisonTypes
 
-import java.util.{ArrayDeque, Deque}
+import java.util.ArrayDeque
 
-class IndentingLexer(private val base: Lexer) extends LexerBase {
+object IndentingLexer {
+  final case class Tok(tpe: IElementType, start: Int, end: Int)
 
-  private final class Tok(val tpe: IElementType, val start: Int, val end: Int)
+  final case class State(
+      buffer: CharSequence,
+      startOffset: Int,
+      endOffset: Int,
+      atLineStart: Boolean,
+      pendingIndent: Option[Int],
+      pendingIndentPos: Option[Int],
+      pendingDedentAnchor: Option[Int],
+      parenDepth: Int
+  )
 
-  private var buffer: CharSequence = ""
-  private var startOffset: Int = 0
-  private var endOffset: Int = 0
+  val emptyState: State =
+    State(
+      buffer = "",
+      startOffset = 0,
+      endOffset = 0,
+      atLineStart = true,
+      pendingIndent = None,
+      pendingIndentPos = None,
+      pendingDedentAnchor = None,
+      parenDepth = 0
+    )
+}
 
-  private val queue: Deque[Tok] = new ArrayDeque[Tok]()
-  private val indentStack: Deque[Integer] = new ArrayDeque[Integer]()
+class IndentingLexer(private val base: Lexer)
+    extends LexerBase {
+  import intellij.unison.IndentingLexer.{Tok, State, emptyState}
 
-  private var atLineStart: Boolean = true
-  private var pendingIndent: java.lang.Integer = null
-  private var parenDepth: Int = 0
+  private val q = new ArrayDeque[Tok]()
+  private val indentStack = new ArrayDeque[Int]()
+
+  private var st: State = emptyState
 
   private var tokenType: IElementType = null
   private var tokenStart: Int = 0
   private var tokenEnd: Int = 0
 
-  private var pendingIndentPos: java.lang.Integer = null      // first space/tab after newline
-  private var pendingDedentAnchor: java.lang.Integer = null   // newline position
-
   override def start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int): Unit = {
-    this.buffer = buffer
-    this.startOffset = startOffset
-    this.endOffset = endOffset
+    st = emptyState.copy(
+      buffer = buffer,
+      startOffset = startOffset,
+      endOffset = endOffset
+    )
 
-    queue.clear()
+    q.clear()
     indentStack.clear()
     indentStack.push(0)
-
-    atLineStart = true
-    pendingIndent = null
-    parenDepth = 0
-    pendingIndentPos = null
-    pendingDedentAnchor = null
 
     base.start(buffer, startOffset, endOffset, initialState)
     pullNext()
   }
 
   override def getState: Int = base.getState
-  override def getBufferSequence: CharSequence = buffer
-  override def getBufferEnd: Int = endOffset
+  override def getBufferSequence: CharSequence = st.buffer
+  override def getBufferEnd: Int = st.endOffset
 
   override def getTokenType: IElementType = tokenType
   override def getTokenStart: Int = tokenStart
@@ -59,28 +73,15 @@ class IndentingLexer(private val base: Lexer) extends LexerBase {
   override def advance(): Unit = pullNext()
 
   private def pullNext(): Unit = {
-    // serve queued synthetic tokens first
-    if (!queue.isEmpty) {
-      val t = queue.removeFirst()
-      setToken(t)
-      return
-    }
+    if (!q.isEmpty) setToken(q.removeFirst())
+    else pullFromBase()
+  }
 
+  private def pullFromBase(): Unit = {
     val t = base.getTokenType
 
-    // end: flush remaining DEDENTs
-    if (t == null) {
-      if (indentStack.size() > 1) {
-        indentStack.pop()
-        val p = Math.max(startOffset, endOffset - 1)
-        setToken(new Tok(UnisonTypes.DEDENT, p, endOffset))
-      } else {
-        tokenType = null
-      }
-      return
-    }
+    if (t == null) { flushDedentsAtEof(); return }
 
-    // split whitespace into WHITE_SPACE / NEWLINE / WHITE_SPACE
     if (t == TokenType.WHITE_SPACE) {
       splitWhitespace(base.getTokenStart, base.getTokenEnd)
       base.advance()
@@ -88,109 +89,114 @@ class IndentingLexer(private val base: Lexer) extends LexerBase {
       return
     }
 
-    // emit INDENT/DEDENT before real token if needed
-    if (parenDepth == 0 && atLineStart && pendingIndent != null) {
-      emitIndentDedent(pendingIndent.intValue())
-      pendingIndent = null
-      atLineStart = false
-
-      if (!queue.isEmpty) {
-        val q = queue.removeFirst()
-        setToken(q)
-        return
+    if (st.parenDepth == 0 && st.atLineStart) {
+      st.pendingIndent.foreach { ind =>
+        emitIndentDedent(ind)
+        st = st.copy(pendingIndent = None, atLineStart = false)
       }
+      if (!q.isEmpty) { setToken(q.removeFirst()); return }
     }
 
-    // emit real token
-    val s = base.getTokenStart
-    val e = base.getTokenEnd
-    val real = new Tok(t, s, e)
-
-    // update paren depth
-    if (t == UnisonTypes.LPAREN || t == UnisonTypes.LPARENSQ || t == UnisonTypes.LBRACE) parenDepth += 1
-    else if (t == UnisonTypes.RPAREN || t == UnisonTypes.RPARENSQ || t == UnisonTypes.RBRACE) parenDepth = Math.max(0, parenDepth - 1)
-
+    val real = Tok(t, base.getTokenStart, base.getTokenEnd)
+    st = st.copy(parenDepth = updateParenDepth(st.parenDepth, t))
     base.advance()
     setToken(real)
   }
 
+  private def flushDedentsAtEof(): Unit = {
+    if (indentStack.size() > 1) {
+      indentStack.pop()
+      val p = Math.max(st.startOffset, st.endOffset - 1)
+      setToken(Tok(UnisonTypes.DEDENT, p, st.endOffset))
+    } else tokenType = null
+  }
+
+  private def updateParenDepth(depth: Int, t: IElementType): Int =
+    if (t == UnisonTypes.LPAREN || t == UnisonTypes.LPARENSQ || t == UnisonTypes.LBRACE) depth + 1
+    else if (t == UnisonTypes.RPAREN || t == UnisonTypes.RPARENSQ || t == UnisonTypes.RBRACE) Math.max(0, depth - 1)
+    else depth
+
   private def splitWhitespace(wsStart: Int, wsEnd: Int): Unit = {
-    // ignore NEWLINE semantics inside (), [], {}
-    if (parenDepth > 0) {
-      queue.addLast(new Tok(TokenType.WHITE_SPACE, wsStart, wsEnd))
+    if (st.parenDepth > 0) {
+      q.addLast(Tok(TokenType.WHITE_SPACE, wsStart, wsEnd))
       return
     }
 
+    val buf = st.buffer
     var segStart = wsStart
     var lastNl = -1
 
     var i = wsStart
     while (i < wsEnd) {
-      val c = buffer.charAt(i)
-      if (c == '\n') {
-        if (segStart < i) queue.addLast(new Tok(TokenType.WHITE_SPACE, segStart, i))
-        queue.addLast(new Tok(UnisonTypes.NEWLINE, i, i + 1))
-        atLineStart = true
+      if (buf.charAt(i) == '\n') {
+        if (segStart < i) q.addLast(Tok(TokenType.WHITE_SPACE, segStart, i))
+        q.addLast(Tok(UnisonTypes.NEWLINE, i, i + 1))
+        st = st.copy(atLineStart = true)
         lastNl = i
         segStart = i + 1
       }
       i += 1
     }
 
-    if (segStart < wsEnd) queue.addLast(new Tok(TokenType.WHITE_SPACE, segStart, wsEnd))
+    if (segStart < wsEnd) q.addLast(Tok(TokenType.WHITE_SPACE, segStart, wsEnd))
 
     if (lastNl >= 0) {
       val tailStart = lastNl + 1
-      pendingIndent = computeIndentWidth(tailStart, wsEnd)
-      pendingIndentPos = firstIndentCharPos(tailStart, wsEnd)
-      pendingDedentAnchor = lastNl
+      val indentW = computeIndentWidth(buf, tailStart, wsEnd)
+      val indentPos = firstIndentCharPos(buf, tailStart, wsEnd)
+
+      st = st.copy(
+        pendingIndent = Some(indentW),
+        pendingIndentPos = Some(indentPos),
+        pendingDedentAnchor = Some(lastNl)
+      )
     }
   }
 
-  private def computeIndentWidth(from: Int, to: Int): java.lang.Integer = {
+  private def computeIndentWidth(buf: CharSequence, from: Int, to: Int): Int = {
     var w = 0
     var i = from
     while (i < to) {
-      buffer.charAt(i) match {
+      buf.charAt(i) match {
         case ' '  => w += 1
-        case '\t' => w += 4 // convention
-        case _    => i = to // break
+        case '\t' => w += 4
+        case _    => return w
       }
       i += 1
     }
     w
   }
 
-  private def emitIndentDedent(newIndent: Int): Unit = {
-    val cur = indentStack.peek().intValue()
-
-    val indentPos = if (pendingIndentPos != null) pendingIndentPos.intValue() else base.getTokenStart
-    val dedentPos = if (pendingDedentAnchor != null) pendingDedentAnchor.intValue() else base.getTokenStart
-
-    if (newIndent > cur) {
-      indentStack.push(newIndent)
-      queue.addLast(new Tok(UnisonTypes.INDENT, indentPos, Math.min(indentPos + 1, endOffset)))
-    } else if (newIndent < cur) {
-      while (indentStack.size() > 1 && indentStack.peek().intValue() > newIndent) {
-        indentStack.pop()
-        queue.addLast(new Tok(UnisonTypes.DEDENT, dedentPos, Math.min(dedentPos + 1, endOffset)))
-      }
-    }
-
-    pendingIndentPos = null
-    pendingDedentAnchor = null
-  }
-
-  private def firstIndentCharPos(from: Int, to: Int): java.lang.Integer = {
+  private def firstIndentCharPos(buf: CharSequence, from: Int, to: Int): Int = {
     var i = from
     while (i < to) {
-      val c = buffer.charAt(i)
-      if (c == ' ' || c == '\t') return i
-      if (c == '\n' || c == '\r') { i += 1; () }
-      else return from
+      buf.charAt(i) match {
+        case ' ' | '\t'  => return i
+        case '\n' | '\r' => // skip
+        case _           => return from
+      }
       i += 1
     }
     from
+  }
+
+  private def emitIndentDedent(newIndent: Int): Unit = {
+    val cur = indentStack.peek()
+
+    val indentPos = st.pendingIndentPos.getOrElse(base.getTokenStart)
+    val dedentPos = st.pendingDedentAnchor.getOrElse(base.getTokenStart)
+
+    if (newIndent > cur) {
+      indentStack.push(newIndent)
+      q.addLast(Tok(UnisonTypes.INDENT, indentPos, Math.min(indentPos + 1, st.endOffset)))
+    } else if (newIndent < cur) {
+      while (indentStack.size() > 1 && indentStack.peek() > newIndent) {
+        indentStack.pop()
+        q.addLast(Tok(UnisonTypes.DEDENT, dedentPos, Math.min(dedentPos + 1, st.endOffset)))
+      }
+    }
+
+    st = st.copy(pendingIndentPos = None, pendingDedentAnchor = None)
   }
 
   private def setToken(t: Tok): Unit = {
