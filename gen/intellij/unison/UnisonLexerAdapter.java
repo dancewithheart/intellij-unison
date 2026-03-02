b90ae79a4aeed73c6220c93f0da15d00f576f836
dancewithheart <dancingwithopenheart@gmail.com>
2026-03-02 18:20:42 +0100

    Java => Scala: UnisonLexerAdapter => UnisonLexerAdapter

--- gen/intellij/unison/UnisonLexerAdapter.java
+++
@@ -1,9 +0,0 @@
-package intellij.unison;
-
-import com.intellij.lexer.FlexAdapter;
-
-public class UnisonLexerAdapter extends IndentingLexer {
-    public UnisonLexerAdapter() {
-        super(new FlexAdapter(new UnisonLexer(null)));
-    }
-}
