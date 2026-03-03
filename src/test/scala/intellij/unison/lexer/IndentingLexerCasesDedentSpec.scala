d01a72954419dac0dbce99e8c4af9cba22472a20
dancewithheart <dancingwithopenheart@gmail.com>
2026-03-03 23:16:18 +0100

    more helpers for tests - use in LexerCaseDedentSpec

--- src/test/scala/intellij/unison/lexer/IndentingLexerCasesDedentSpec.scala
+++ src/test/scala/intellij/unison/lexer/IndentingLexerCasesDedentSpec.scala
@@ -1,66 +1,51 @@
 package intellij.unison.lexer
 
-import com.intellij.lexer.FlexAdapter
+import zio.test._
+import com.intellij.lexer.{FlexAdapter, Lexer}
 import com.intellij.psi.TokenType
-import com.intellij.psi.tree.IElementType
 import intellij.unison.{IndentingLexer, UnisonLexer}
 import intellij.unison.language.psi.UnisonTypes
-import zio.test._
+import intellij.unison.lexer.IndentingLexerTestSupport._
 
 object IndentingLexerCasesDedentSpec extends ZIOSpecDefault {
 
-  private def lex(code: String): Vector[(IElementType, Int, Int, String)] = {
-    val base  = new FlexAdapter(new UnisonLexer(null))
-    val lexer = new IndentingLexer(base)
-    lexer.start(code, 0, code.length, 0)
+  private def mk: Lexer =
+    new IndentingLexer(new FlexAdapter(new UnisonLexer(null)))
 
-    val b = Vector.newBuilder[(IElementType, Int, Int, String)]
-    while (lexer.getTokenType != null) {
-      val t = lexer.getTokenType
-      val s = lexer.getTokenStart
-      val e = lexer.getTokenEnd
-      b += ((t, s, e, code.substring(s, e)))
-      lexer.advance()
-    }
-    b.result()
-  }
+  private def dump(out: Vector[Lexed]): String =
+    "\nTOKENS:\n" + IndentingLexerTestSupport.render(out)
 
   override def spec: Spec[TestEnvironment, Any] =
     suite("IndentingLexerCasesDedent")(
       test("casesBlockEmitsTwoDedentsBeforeTopLevelX") {
         val code =
-          "mkErr : Exception\n" +
-            "mkErr = failure \"Zero was found\"\n" +
-            "\n" +
-            "nonZero : Nat ->  { Exception } Nat\n" +
-            "nonZero = cases\n" +
-            "  n\n" +
-            "    | n Nat.== 0  -> mkErr\n" +
-            "    | otherwise   -> n\n" +
-            "\n" +
-            "x = catch do nonZero 0\n"
+          """|mkErr : Exception
+             |mkErr = failure "Zero was found"
+             |
+             |nonZero : Nat ->  { Exception } Nat
+             |nonZero = cases
+             |  n
+             |    | n Nat.== 0  -> mkErr
+             |    | otherwise   -> n
+             |
+             |x = catch do nonZero 0
+             |""".stripMargin
 
-        val toks = lex(code)
+        val toks = lexAll(code, mk, includeWhitespace = true)
 
-        // Find the 'x' identifier token
-        val xIdx = toks.indexWhere { case (t, _, _, txt) =>
-          t == UnisonTypes.IDENTIFIER_TOKEN && txt == "x"
-        }
+        val xIdx = toks.indexWhere(t => t.tpe == UnisonTypes.IDENTIFIER_TOKEN && t.text == "x")
 
-        // Count consecutive DEDENT tokens immediately before x (ignoring whitespace/newlines)
         val dedentsBeforeX =
           if (xIdx < 0) -1
-          else {
-            var i = xIdx - 1
-            while (i >= 0 && (toks(i)._1 == TokenType.WHITE_SPACE || toks(i)._1 == UnisonTypes.NEWLINE)) i -= 1
+          else
+            countDedentsImmediatelyBefore(
+              tokens = toks,
+              index = xIdx,
+              isSkippable = t => t.tpe == TokenType.WHITE_SPACE || t.tpe == UnisonTypes.NEWLINE
+            )
 
-            var dedents = 0
-            while (i >= 0 && toks(i)._1 == UnisonTypes.DEDENT) { dedents += 1; i -= 1 }
-            dedents
-          }
-
-        assertTrue(xIdx >= 0) &&
-          assertTrue(dedentsBeforeX == 2)
+        assertTrue(xIdx >= 0 && dedentsBeforeX == 2)
+          .label(s"xIdx=$xIdx dedentsBeforeX=$dedentsBeforeX" + dump(toks))
       }
     )
 }
